16ecd1063abd078b6f5c0017f0b82a9ceb697beb
suprabhatkumar <suprabhat2018@gmail.com>
2026-05-29 00:44:07 +0530

    feat: fetch text from PDFs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medbot",
+    version="0.1.0",
+    author="mercuryranger",
+    packages=find_packages(),
+    install_requires=[]
+)
